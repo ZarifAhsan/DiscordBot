2fb6a8ef96e6b480ed5442c19598a1940ee676a2
ZarifAhsan <mdzarifahsan@gmail.com>
2021-09-21 02:14:56 +0600

    Mostly done

--- bot/main.py
+++ bot/main.py
@@ -4,10 +4,10 @@ import tune
 
 cog = [music]
 
+client = commands.Bot(command_prefix='@', intents = discord.Intents.all())
+
 for i in range(len(cogs)):
     cogs[i].setup()
 
-
-client = commands.Bot(command_prefix='@', intents = discord.Intents.all())
 
 client.run(ODg5NTkxNDk2MDQ5ODE5NjQ4.YUjepA.lSQ1C8Dw4t2NEHDsy2cBDPARhyM)
