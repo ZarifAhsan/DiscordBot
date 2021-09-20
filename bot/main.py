25e0f325443189d9b95a6fa5d56d1045025bd882
ZarifAhsan <mdzarifahsan@gmail.com>
2021-09-21 01:43:05 +0600

    main client init

---
+++ bot/main.py
@@ -0,0 +1,6 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='@', intents = discord.Intents.all())
+
+client.run(ODg5NTkxNDk2MDQ5ODE5NjQ4.YUjepA.lSQ1C8Dw4t2NEHDsy2cBDPARhyM)
