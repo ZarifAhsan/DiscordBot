6c24e3bda41fddb7939c96b174a2c8bb21de6f3c
ZarifAhsan <mdzarifahsan@gmail.com>
2021-09-21 01:59:59 +0600

    some main functions added

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+import tune
+
+cog = [music]
+
+for i in range(len(cogs)):
+    cogs[i].setup()
+
 
 client = commands.Bot(command_prefix='@', intents = discord.Intents.all())
 
--- bot/tune.py
+++ bot/tune.py
@@ -7,5 +7,21 @@ class tune():
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel :( ")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+        @commands.command()
+        async def disconnect(self, ctx):
+            await ctx.voice_client.disconnect()
+
+
+
 def setup(client):
     client.add_cog(music.client)
