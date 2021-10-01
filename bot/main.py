9389ef339e582023a3602e030c4feede4e807746
Zarif Bin Ahsan <61982553+ZarifAhsan@users.noreply.github.com>
2021-10-01 23:57:00 +0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -10,5 +10,5 @@ intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-
-client.run("ODg5NTkxNDk2MDQ5ODE5NjQ4.YUjepA.lSQ1C8Dw4t2NEHDsy2cBDPARhyM")
+    
+client.run("ODg5NTkxNDk2MDQ5ODE5NjQ4.YUjepA.lSQ1C8Dw4t2NEHDsy2cBDPARhyM") # Add your discord bot code (The one here is fake :P)
